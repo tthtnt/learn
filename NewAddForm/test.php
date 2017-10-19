daab562655b67794011300a47c981fe30094827d
SiJie Liu <SiJie.Liu@concentrix.com>
2017-10-19 16:49:24 +0800

    加入form新增部分属性

---
+++ NewAddForm/test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>这是第一个测试页</title>
+</head>
+<body>
+	<?php
+		$s1 = $_POST['s1'];
+		$s2 = $_POST['s2'];
+		echo $s1,$s2;
+	?>
+</body>
+</html>
